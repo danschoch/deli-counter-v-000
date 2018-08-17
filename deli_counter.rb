9a37c4ada833c6e2ed21f128e29c70d8b69b0488
Daniel Schoch <d.schoch90@gmail.com>
2018-08-17 00:05:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli = []
+
+def line (katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    line_is = "The line is currently:"
+    katz_deli.each_with_index do |customer, index|
+      line_is + "#{index + 1}. #{customer}"
+    end
+    puts 
+  end
