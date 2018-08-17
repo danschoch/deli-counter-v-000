a9b8915335252cca029441b02cbe3a311b4fe133
Daniel Schoch <d.schoch90@gmail.com>
2018-08-17 00:18:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,10 +12,11 @@ def line (katz_deli)
   end
 end
 
-def take_a_number
-  
+def take_a_number (katz_deli, customer)
+  katz_deli << customer
+  puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving
-  
+
 end
